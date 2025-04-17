edce03019c1b8c84b68acd77b19eca95d66210bf
Riteshpandey9693 <pandeyritesh9693@gmail.com>
2025-04-17 13:10:49 +0530

    fix the bug

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,5 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  base: process.env.VITE_BASE_PATH || "/Tasty_bite", // Default to root if VITE_BASE_PATH is not set
+  base: process.env.VITE_BASE_PATH || "/", // Default to root if VITE_BASE_PATH is not set
 })
